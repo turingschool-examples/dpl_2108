3ab9ac99628e25d8658ba1d810310164c3b94fd6
Philip Medlin <phmedlin@gmail.com>
2021-09-16 14:26:53 -0600

    Feat: add publication time frame method

--- lib/library.rb
+++ lib/library.rb
@@ -17,7 +17,13 @@ class Library
   end
 
   def publication_time_frame_for(author)
-
+    time_frame = {:start =>
+      (author.books.min_by do |book|
+        book.publication_year.to_i
+      end).publication_year.to_s ,
+      :end => (author.books.max_by do |book|
+        book.publication_year.to_i
+      end).publication_year.to_s}
+    time_frame
   end
-
 end
