e3b8f5835801051d8082b0b3986012c18f8ed425
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-16 13:27:53 -0600

    Add #name test

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -4,12 +4,29 @@ require './lib/author'
 
 describe Author do
 
+  before(:each) do
+
+    @author_info = {
+                    first_name: "Charlotte",
+                    last_name: "Bronte"
+                   }
+
+  end
+
   describe '#initialize' do
     it 'creates an instance of Author' do
-      author = Author.new
+      author = Author.new(@author_info)
 
       expect(author).to be_an_instance_of(Author)
     end
+  end
+
+  describe '#name' do
+    it 'returns the full author name' do
+      author = Author.new(@author_info)
+
+      expect(author.name).to eq("Charlotte Bronte")
+    end 
   end
 
 end
