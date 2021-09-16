2a8f5dd28f7b00e8b59d6ce98bacf3c114bf969d
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-16 14:25:04 -0600

    Feat - Add methods for checking out and returning books

--- lib/library.rb
+++ lib/library.rb
@@ -1,12 +1,14 @@
 class Library
   attr_reader :name,
               :books,
-              :authors
+              :authors,
+              :checked_out_books
 
   def initialize(name)
     @name = name
     @books = []
     @authors = []
+    @checked_out_books = []
   end
 
   def add_author(author)
@@ -21,5 +23,22 @@ class Library
       start: author.min_max_year[0],
       end:   author.min_max_year[1]
     }
+  end
+
+  def checkout(book)
+    if books.include?(book)
+      checked_out_books << book
+      books.delete(book)
+      true
+    else
+      books.include?(book)
+    end
+  end
+
+  def return(book)
+    if checked_out_books.include?(book)
+      books << book
+      checked_out_books.delete(book)
+    end
   end
 end
