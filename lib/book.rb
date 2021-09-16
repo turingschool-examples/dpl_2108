715c046b6fdab17a38c07c8cbad22e7bc31c3777
Andrew Patterson <andrew.patterson747@gmail.com>
2021-09-16 14:10:23 -0500

    Initial commit

---
+++ lib/book.rb
@@ -0,0 +1,11 @@
+class Book
+  attr_reader :title,
+              :publication_date
+
+  def initialize(data)
+    @author_first_name = data[:author_first_name]
+    @author_last_name  = data[:author_last_name]
+    @title             = data[:title]
+    @publication_date  = data[:publication_date]
+  end
+end
---
+++ spec/book_spec.rb
@@ -0,0 +1,24 @@
+require './lib/book'
+
+RSpec.describe Book do
+  it 'exists' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book).to be_a(Book)
+  end
+
+  it 'has a title' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book.title).to eq("To Kill a Mockingbird")
+  end
+
+  it 'has an author' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book.author).to eq("Harper Lee")
+  end
+end
+
+# book.publication_year
+# #=> "1960"
