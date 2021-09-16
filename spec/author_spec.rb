3efe9c9014c438ac38833cc6961328bf889152f7
Devin Pile <devinpile@gmail.com>
2021-09-16 14:16:31 -0600

    Add tests for #write and #publish

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -18,4 +18,21 @@ RSpec.describe Author do
     expect(@charlotte_bronte.name).to eq("Charlotte Bronte")
     expect(@charlotte_bronte.books). to eq([])
   end
+
+  it 'can write a book' do
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    expect(jane_eyre.title).to eq("Jane Eyre")
+  end
+
+  it 'can publish a book' do
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @charlotte_bronte.publish(jane_eyre)
+    expect(@charlotte_bronte.books).to eq([jane_eyre])
+
+    villette = @charlotte_bronte.write("Villette", "1853")
+    @charlotte_bronte.publish(villette)
+    expect(@charlotte_bronte.books).to eq([jane_eyre, villette])
+  end
+
+
 end
