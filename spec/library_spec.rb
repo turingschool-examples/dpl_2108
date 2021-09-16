7c45723513294aedeb333f9008541d06997bdee0
Chaz Simons <chaz.simons@gmail.com>
2021-09-16 12:48:09 -0700

    Test: add author method test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -15,4 +15,22 @@ describe Library do
     expect(@dpl.books).to eq([])
     expect(@dpl.authors).to eq([])
   end
+
+  it 'can add authors' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    charlotte_bronte.publish(jane_eyre)
+    charlotte_bronte.publish(professor)
+    charlotte_bronte.publish(villette)
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    harper_lee.publish(mockingbird)
+
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+
+    expect(@dpl.authors).to eq([charlotte_bronte, harper_lee])
+  end
 end
