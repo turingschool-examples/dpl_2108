2ca86ead89be38c7074e9706ac816032ef08c6d8
Philip Medlin <phmedlin@gmail.com>
2021-09-16 13:41:03 -0600

    Fix refactor book_spec

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -8,7 +8,7 @@ describe Book do
     book = Book.new({author_first_name: "Harper", author_last_name: "Lee",
             title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
-    expect(whatever).to be_an_instance_of(Whatever)
+    expect(book).to be_an_instance_of(Book)
   end
 
   it 'has readable attributes' do
