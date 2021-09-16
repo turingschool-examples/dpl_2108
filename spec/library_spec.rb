82784d1d0f384565918ed066685f4ddcbcc8def0
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-16 13:56:07 -0600

    Test - Finalized tests for library class functionality

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -6,6 +6,16 @@ require './lib/library'
 describe Library do
   before(:each) do
     @dpl = Library.new("Denver Public Library")
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @professor = @charlotte_bronte.write("The Professor", "1857")
+    @villette = @charlotte_bronte.write("Villette", "1853")
+    @charlotte_bronte.publish(@jane_eyre)
+    @charlotte_bronte.publish(@professor)
+    @charlotte_bronte.publish(@villette)
+    @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    @mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    @harper_lee.publish(@mockingbird)
   end
 
   it 'is an instance of Library' do
@@ -31,6 +41,26 @@ describe Library do
     it 'prints all authors with books in library, or empty array if no authors' do
 
       expect(@dpl.authors).to eq([])
+    end
+  end
+
+  describe '#add_author' do
+    it 'adds author and their books to library' do
+      @dpl.add_author(@charlotte_bronte)
+      @dpl.add_author(@harper_lee)
+
+      expect(@dpl.authors).to eq([@charlotte_bronte, @harper_lee])
+      expect(@dpl.books).to eq([@jane_eyre, @professor, @villette, @mockingbird])
+    end
+  end
+
+  describe '#publication_time_frame_for' do
+    it 'returns the start and end time of author publication' do
+      @dpl.add_author(@charlotte_bronte)
+      @dpl.add_author(@harper_lee)
+
+      expect(@dpl.publication_time_frame_for(@charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
+      expect(@dpl.publication_time_frame_for(@harper_lee)).to eq({:start=>"1960", :end=>"1960"})
     end
   end
 end
