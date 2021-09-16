c37ea8aad328302a6f34724d1348a6dbf1193256
Paul Leonard <paulpaulleonard@gmail.com>
2021-09-16 15:22:47 -0400

    Finish iteration 1

---
+++ spec/book_spec.rb
@@ -0,0 +1,23 @@
+require './lib/book'
+
+RSpec.describe "Book" do
+  before(:each) do
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  end
+
+  it "exist" do
+    expect(@book).to be_a(Book)
+  end
+
+  it "has a title" do
+    expect(@book.title).to eq("To Kill a Mockingbird")
+  end
+
+  it "has an author" do
+    expect(@book.author).to eq("Harper Lee")
+  end
+
+  it "has a publication year" do
+    expect(@book.publication_year).to eq("1960")
+  end
+end
