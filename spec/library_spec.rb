332ad29801a29b68b903deb062e9db1213a4f50a
Devin Pile <devinpile@gmail.com>
2021-09-16 14:21:36 -0600

    Create initial tests for library spec

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -3,6 +3,16 @@ require './lib/author'
 
 RSpec.describe Library do
   before(:each) do
-    dpl = Library.new("Denver Public Library")
-  end 
+    @dpl = Library.new("Denver Public Library")
+  end
+
+  it 'exists' do
+    expect(@dpl).to be_an_instance_of Library
+  end
+
+  it 'has readable attributes' do
+    expect(@dpl.name).to eq("Denver Public Library")
+    expect(@dpl.books).to eq([])
+    expect(@dpl.authors).to eq([])
+  end
 end
