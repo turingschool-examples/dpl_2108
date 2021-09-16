06db0682b536f1eb4f26e065d9db35d1f08997b0
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-16 15:48:41 -0400

    Add final methods for author class

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,25 @@
 class Author
   attr_reader :first_name,
-              :last_name
-              
+              :last_name,
+              :books
+
   def initialize(author_info)
     @author_info = author_info
     @first_name = author_info[:first_name]
-    @last_name = author_info[:first_name]
+    @last_name = author_info[:last_name]
+    @books = []
+
+  end
+
+  def name
+    @first_name + " " + @last_name
+  end
+
+  def write(title, publication_date)
+    book = Book.new({title: title, publication_date: publication_date})
+  end
+
+  def publish(book)
+    @books << (book)
   end
 end
