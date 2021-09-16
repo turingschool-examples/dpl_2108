c87bc772e5aa93605577dd8a54b62dd05f447d8d
Philip Medlin <phmedlin@gmail.com>
2021-09-16 13:45:57 -0600

    Feat: Create library class

---
+++ lib/library.rb
@@ -0,0 +1,19 @@
+class Library
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @books = []
+    @authors = []
+  end
+
+  def add_author(author)
+
+  end
+
+  def publication_time_frame_for(author)
+
+  end
+  
+end
