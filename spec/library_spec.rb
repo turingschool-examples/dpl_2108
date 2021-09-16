5141ea4a1d0e458b307d9a8200baa531094cf155
Gregory Fischer <gwhoisj@gmail.com>
2021-09-16 16:36:43 -0400

    Add upset comments

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -51,9 +51,10 @@ describe Library do
     @dpl.add_author(@charlotte_bronte)
     @dpl.add_author(@harper_lee)
 
-    #but these somehow don't?
+    #but these somehow don't? I don't understand what's different here.
     expect(@dpl.publication_time_frame_for(@charlotte_bronte)).to eq {start:"1847", end:"1857"}
     expect(@dpl.publication_time_frame_for(@harper_lee)).to eq {start:"1960", end:"1960"}
+    #why does it work in pry but not here...
   end
 
   it "#checkout" do
