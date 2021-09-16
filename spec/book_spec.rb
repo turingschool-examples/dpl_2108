0b706de0b5f24b12e448ee7eeb03efb2c56df79b
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-16 13:15:35 -0600

    Create readable attr test

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,11 +3,31 @@ require './lib/book'
 
 describe Book do
 
+  before(:each) do
+
+  @book_info = {
+                author_first_name: "Harper",
+                author_last_name: "Lee",
+                title: "To Kill a Mockingbird",
+                publication_date: "July 11, 1960"
+               }
+
+  end
+
   describe '#initialize' do
     it 'creates an instance of Book' do
-      book = Book.new
+      book = Book.new(@book_info)
 
       expect(book).to be_an_instance_of(Book)
+    end
+
+    it 'has readable attributes' do
+      book = Book.new(@book_info)
+
+      expect(book.author_first_name).to eq("Harper")
+      expect(book.author_last_name).to eq("Lee")
+      expect(book.title).to eq("To Kill a Mockingbird")
+      expect(book.publication_year).to eq('1960')
     end
   end
 
