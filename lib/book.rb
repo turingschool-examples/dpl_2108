9553b687f475ff498f19e6c3a9becff884d2e4fe
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-16 14:03:47 -0600

    Create #pub_year

--- lib/book.rb
+++ lib/book.rb
@@ -9,7 +9,14 @@ class Book
     @author_first_name = book_info[:author_first_name]
     @author_last_name = book_info[:author_last_name]
     @title = book_info[:title]
-    @publication_year = book_info[:publication_date].split(', ')[1]
+    @publication_year = pub_year(book_info[:publication_date])
+  end
+
+  def pub_year(pub_info)
+    #date = ''
+    pub_info.split(', ').find do |date_frag|
+      date_frag.length <= 4
+    end
   end
 
 end
