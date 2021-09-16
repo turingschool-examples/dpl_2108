827a1f7edd03a41f52d347ba4c450b81867310fe
Micha Bernhard <mib70@pitt.edu>
2021-09-16 14:17:25 -0600

    Feat - Write methods for iteration 1

---
+++ lib/book.rb
@@ -0,0 +1,21 @@
+class Book
+  attr_reader :title,
+              :author,
+              :publication_year
+
+  def initialize(data)
+    @title = data[:title]
+    @author = (data[:author_first_name] + " " + data[:author_last_name])
+    @publication_date = data[:publication_date]
+    @publication_year = only_year
+  end
+
+  def only_year
+    if @publication_date.include?(',')
+      year = @publication_date.partition(',').last
+      year.strip
+    else
+      @publication_date
+    end
+  end
+end
