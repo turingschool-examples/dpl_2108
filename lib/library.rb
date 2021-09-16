f4bbb6ca7a9bf56ff2ff63c6b2456581e36b21e8
Micha Bernhard <mib70@pitt.edu>
2021-09-16 14:50:34 -0600

    Feat - Write all methods for Iteration 4 (some not working)

--- lib/library.rb
+++ lib/library.rb
@@ -1,12 +1,15 @@
 class Library
   attr_reader :name,
               :books,
-              :authors
+              :authors,
+              :checked_out_books
 
   def initialize(name)
     @name = name
     @authors = []
     @books = []
+    @checked_out_books = []
+    @most_popular = nil
   end
 
   def add_author(author)
@@ -29,5 +32,50 @@ class Library
       start: minimum.to_s,
     end: maximum.to_s
     }
+  end
+
+  def checkout(book)
+    if @books.include?(book)
+      if !@checked_out_books.include?(book)
+        @checked_out_books << book
+        popularity(book)
+        return true
+      else
+        false
+      end
+    else
+      false
+    end
+  end
+
+  def return(book)
+    @checked_out_books.delete(book)
+  end
+
+  def popularity(book)
+    jane_eyre_popularity = 0
+    professor_popularity = 0
+    villette_popularity = 0
+    mockingbird_popularity = 0
+
+    if book == @jane_eyre
+      jane_eyre_popularity += 1
+    elsif book == @professor
+      professor_popularity += 1
+    elsif book == @villette
+      villette_popularity += 1
+    elsif book == @mockingbird
+      mockingbird_popularity += 1
+    end
+
+    all_books = [jane_eyre_popularity, professor_popularity ,villette_popularity, mockingbird_popularity]
+    @most_popular = all_books.max
+  end
+
+  def most_popular_book
+    @most_popular # this obviously does not work on many levels because
+                  # even if it would work the way I intended it would
+                  # return an integer and not the book, but alas there is
+                  # no more time and I must refactor the working code.
   end
 end
