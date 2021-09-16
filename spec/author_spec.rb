cab12c73503ac42edb2aa83716c568a7cbd3eba7
Philip Medlin <phmedlin@gmail.com>
2021-09-16 13:26:59 -0600

    Test: create write method test

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -14,10 +14,16 @@ describe Author do
     charlotte_bronte = Author.new({first_name: "Charlotte",
        last_name: "Bronte"})
 
-    eexpect(charlotte_bronte.name).to eq("Charlotte Bronte")
+    expect(charlotte_bronte.name).to eq("Charlotte Bronte")
     expect(charlotte_bronte.books).to eq([])
   end
 
-  xit 'does things' do
+  xit 'can write a book' do
+    charlotte_bronte = Author.new({first_name: "Charlotte",
+       last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    expect(jane_eyre).to_be_an_instance_of(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre") 
   end
 end
