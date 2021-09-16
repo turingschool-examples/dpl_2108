1f21abb096b8a4f8c3f763b8a4b0773521a32221
Andrew Patterson <andrew.patterson747@gmail.com>
2021-09-16 14:15:14 -0500

    Complete Iteration 1

--- lib/book.rb
+++ lib/book.rb
@@ -8,4 +8,12 @@ class Book
     @title             = data[:title]
     @publication_date  = data[:publication_date]
   end
+
+  def author
+    "#{@author_first_name} #{@author_last_name}"
+  end
+
+  def publication_year
+    @publication_date[-4..-1]
+  end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -18,7 +18,10 @@ RSpec.describe Book do
 
     expect(book.author).to eq("Harper Lee")
   end
-end
 
-# book.publication_year
-# #=> "1960"
+  it 'has a publication_year' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book.publication_year).to eq("1960")
+  end
+end
