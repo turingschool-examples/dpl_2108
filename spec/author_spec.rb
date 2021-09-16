a3cfca4e38094e73467deb10bb4af1d12a3a357e
Philip Medlin <phmedlin@gmail.com>
2021-09-16 13:34:49 -0600

    Test: Create publish test author_spec

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -18,12 +18,31 @@ describe Author do
     expect(charlotte_bronte.books).to eq([])
   end
 
-  xit 'can write a book' do
+  it 'can write a book' do
     charlotte_bronte = Author.new({first_name: "Charlotte",
        last_name: "Bronte"})
     jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
 
-    expect(jane_eyre).to_be_an_instance_of(Book)
-    expect(jane_eyre.title).to eq("Jane Eyre") 
+    expect(jane_eyre.title).to eq("Jane Eyre")
   end
+
+  it 'can publish a book' do
+    charlotte_bronte = Author.new({first_name: "Charlotte",
+       last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    charlotte_bronte.publish(jane_eyre)
+
+    expect(charlotte_bronte.books).to eq(jane_eyre)
+
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    harlotte_bronte.publish(villette)
+
+    expect(charlotte_bronte.books).to eq([jane_eyre, villette])
+  end
+
+
+
+
 end
