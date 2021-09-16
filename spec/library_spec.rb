77954edc39bd31a219a8b134409fb250eef3496e
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-16 14:57:10 -0600

    Add #publication_time_frame_for test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -51,4 +51,25 @@ describe Library do
     end
   end
 
+  describe '#publication_time_frame_for' do
+    it 'returns the date range of publication for an author' do
+      dpl = Library.new("Denver Public Library")
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      professor = charlotte_bronte.write("The Professor", "1857")
+      villette = charlotte_bronte.write("Villette", "1853")
+      charlotte_bronte.publish(jane_eyre)
+      charlotte_bronte.publish(professor)
+      charlotte_bronte.publish(villette)
+      harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+      harper_lee.publish(mockingbird)
+      dpl.add_author(charlotte_bronte)
+      dpl.add_author(harper_lee)
+
+      expect(dpl.publication_time_frame_for(charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
+      expect(dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
+    end
+  end
+
 end
