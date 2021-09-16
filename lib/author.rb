2e5cfa9e2bfcc69939768c0bac9e178f2a4ea04f
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-16 13:28:08 -0600

    Create #name

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,12 @@
 class Author
 
+  def initialize(author_info)
+    @first_name = author_info[:first_name]
+    @last_name = author_info[:last_name]
+  end
 
+  def name
+    @first_name + " " + @last_name
+  end
 
 end
