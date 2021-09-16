f10b67b579064e837130b75dc6964b42520c128d
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-16 13:31:28 -0600

    Create #books

--- lib/author.rb
+++ lib/author.rb
@@ -3,10 +3,15 @@ class Author
   def initialize(author_info)
     @first_name = author_info[:first_name]
     @last_name = author_info[:last_name]
+    @book_array = []
   end
 
   def name
     @first_name + " " + @last_name
+  end
+
+  def books
+    @book_array
   end
 
 end
