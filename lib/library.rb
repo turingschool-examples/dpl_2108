e94400c00640fac60092e3140e26732a95a50f46
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-16 14:42:38 -0600

    Refactor #books

--- lib/library.rb
+++ lib/library.rb
@@ -1,17 +1,25 @@
 class Library
 
   attr_reader :name,
-              :books,
               :authors
 
   def initialize(name)
     @name = name
-    @books = []
+    @book_array = []
     @authors = []
   end
 
   def add_author(name)
     @authors.push(name)
+  end
+
+  def books
+    @authors.each do |author|
+      author.books.each do |book|
+        @book_array.push(book)
+      end
+    end
+    @book_array
   end
 
 end
