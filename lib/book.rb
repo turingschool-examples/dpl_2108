2f2b74272c716707b69d4bb3ba868a8123a28acc
Sam Devine <samuel.will.devine@gmail.com>
2021-09-16 14:25:27 -0600

    feat: Iteration 4 (#checkout, #return)

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,13 @@
 class Book
-  attr_reader :title
+  attr_reader :title,
+              :checked_out_count
 
   def initialize(info)
-    @title            = info[:title]
-    @first_name       = info[:author_first_name]
-    @last_name        = info[:author_last_name]
-    @publication_date = info[:publication_date]
+    @title              = info[:title]
+    @first_name         = info[:author_first_name]
+    @last_name          = info[:author_last_name]
+    @publication_date   = info[:publication_date]
+    @checked_out_count  = 0
   end
 
   def author
@@ -14,5 +16,9 @@ class Book
 
   def publication_year
     @publication_date[-4..-1]
+  end
+
+  def checked_out
+    @checked_out_count += 1
   end
 end
--- lib/library.rb
+++ lib/library.rb
@@ -4,12 +4,14 @@ require './lib/author'
 class Library
   attr_reader :name,
               :authors,
-              :books
+              :books,
+              :checked_out_books
 
   def initialize(name)
-    @name     = name
-    @books    = []
-    @authors  = []
+    @name              = name
+    @books             = []
+    @authors           = []
+    @checked_out_books = []
   end
 
   def add_author(author)
@@ -35,5 +37,27 @@ class Library
     last_book_year = last_book(author).publication_year
 
     {start: first_book_year, end: last_book_year}
+  end
+
+  def checkout(book)
+    if @checked_out_books.include?(book)
+      false
+    elsif !@books.include?(book)
+      false
+    else
+      book.checked_out
+      @checked_out_books << book
+      true
+    end
+  end
+
+  def return(book)
+    @checked_out_books.delete(book)
+  end
+
+  def most_popular_book
+    @books.max_by do |book|
+      book.checked_out_count
+    end
   end
 end
