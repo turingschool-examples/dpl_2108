583edcc09fa87a96d06a2adb4a74b3530f572a45
Micha Bernhard <mib70@pitt.edu>
2021-09-16 14:49:58 -0600

    Test - Write all tests for iteration 4

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -37,12 +37,47 @@ describe Library do
     it 'has authors' do
       expect(@dpl.authors).to include(@charlotte_bronte, @harper_lee)
     end
+
+    it 'has a list of checked out books' do
+      expect(@dpl.checked_out_books).to eq([])
+    end
   end
 
   describe '#publication_time_frame_for' do
     it 'returns a hash of the time frame' do
       expect(@dpl.publication_time_frame_for(@charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
       expect(@dpl.publication_time_frame_for(@harper_lee)).to eq({:start=>"1960", :end=>"1960"})
+    end
+  end
+
+  describe '#checkout' do
+    it 'checks out a book if it exists' do
+      expect(@dpl.checkout(@jane_eyre)).to eq true
+      expect(@dpl.checkout('this isnt a book')).to eq false
+      expect(@dpl.checkout(@jane_eyre)).to eq false
+      @dpl.checkout(@villette)
+      expect(@dpl.checked_out_books.length).to eq(2)
+    end
+  end
+
+  describe '#return' do
+    it 'removes the book from checked_out_books' do
+      @dpl.checkout(@mockingbird)
+      @dpl.return(@mockingbird)
+      expect(@dpl.checked_out_books).to eq([])
+    end
+  end
+
+  describe '#most_popular_book' do
+    xit 'returns the book that has been checked out the most' do
+      10.times do
+        @dpl.checkout(@mockingbird)
+        @dpl.return(@mockingbird)
+      end
+      @dpl.checkout(@villette)
+      @dpl.return(@villette)
+
+      expect(@dpl.most_popular_book).to eq(@mockingbird)
     end
   end
 end
