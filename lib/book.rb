0c3296695f2695c094ebb0cd757fb5e9210502ec
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-16 13:15:52 -0600

    Create attr_reader

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,15 @@
 class Book
 
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_year
+
+  def initialize(book_info)
+    @author_first_name = book_info[:author_first_name]
+    @author_last_name = book_info[:author_last_name]
+    @title = book_info[:title]
+    @publication_year = book_info[:publication_date].split(', ')[1]
+  end
 
 end
