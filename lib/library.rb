a1fe532a0bd59d26119b08482366919eb951cf9a
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-16 14:57:49 -0600

    Create #publication_time_frame_for

--- lib/library.rb
+++ lib/library.rb
@@ -22,4 +22,19 @@ class Library
     @book_array
   end
 
+  def publication_time_frame_for(author)
+    return_hash = {}
+    min_date = '9999999999999'
+    max_date = ''
+    author.books.each do |book|
+      min_date = book.publication_year if book.publication_year.to_i < min_date.to_i
+    end
+    return_hash[:start] = min_date
+    author.books.each do |book|
+      max_date = book.publication_year if book.publication_year.to_i > max_date.to_i
+    end
+    return_hash[:end] = max_date
+    return_hash
+  end
+
 end
