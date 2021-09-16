84b50cd719f38fcf3d82720a10a392140d83393b
Chaz Simons <chaz.simons@gmail.com>
2021-09-16 13:02:36 -0700

    Test: add test for publication time frame

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -50,4 +50,27 @@ describe Library do
 
     expect(@dpl.books).to eq([jane_eyre, professor, villette, mockingbird])
   end
+
+  it '#publication_time_frame_for' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    charlotte_bronte.publish(jane_eyre)
+    charlotte_bronte.publish(professor)
+    charlotte_bronte.publish(villette)
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    harper_lee.publish(mockingbird)
+
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+
+    author_1 = @dpl.publication_time_frame_for(charlotte_bronte)
+    expect(author_1).to eq({:start=>"1847", :end=>"1857"})
+
+    author_2 = @dpl.publication_time_frame_for(harper_lee)
+
+    expect(author_2).to eq({:start=>"1960", :end=>"1960"})
+  end
 end
