fc1dc68a8f24d2fb417f33bae05f5b6aedde50e4
Devin Pile <devinpile@gmail.com>
2021-09-16 14:16:59 -0600

    Create methods to pass #write and #publish

--- lib/author.rb
+++ lib/author.rb
@@ -6,4 +6,15 @@ class Author
     @name = "#{info[:first_name]} #{info[:last_name]}"
     @books = []
   end
+
+  def write(book_title, pub_date)
+    Book.new({
+                title: book_title,
+                publication_date: pub_date
+              })
+  end
+
+  def publish(book)
+    @books << book
+  end
 end
