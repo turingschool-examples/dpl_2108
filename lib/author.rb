ac9e25e38653ae0c2d6431cd2d32b0136c250ecd
Micha Bernhard <mib70@pitt.edu>
2021-09-16 14:17:54 -0600

    Feat - Write methods for iteration 2

---
+++ lib/author.rb
@@ -0,0 +1,23 @@
+class Author
+  attr_reader :name,
+              :books
+
+  def initialize(data)
+    @name = (data[:first_name] + " " + data[:last_name])
+    @books = []
+  end
+
+  def write(str1, str2)
+    attributes = {
+      author_first_name: (@name.split[0]),
+      author_last_name: (@name.split[1]),
+      title: str1,
+      publication_date: str2
+    }
+    Book.new(attributes)
+  end
+
+  def publish(book)
+    @books << book
+  end
+end
