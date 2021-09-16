c0b1d67216994e5771ef96291d09b9b7e04feed0
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-16 14:14:07 -0600

    Add attr reader test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -7,9 +7,17 @@ describe Library do
 
   describe '#initialize' do
     it 'creates an instance of Library' do
-      library = Library.new
+      dpl = Library.new("Denver Public Library")
 
-      expect(library).to be_an_instance_of(Library)
+      expect(dpl).to be_an_instance_of(Library)
+    end
+
+    it 'has readable attributes' do
+      dpl = Library.new("Denver Public Library")
+
+      expect(dpl.name).to eq("Denver Public Library")
+      expect(dpl.books).to eq([])
+      expect(dpl.authors).to eq([])
     end
   end
 
