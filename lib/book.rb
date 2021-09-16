8b8161804191b146336c7862302f0501fe5770a2
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-16 13:17:58 -0600

    Feat - Finalized creation of book class

---
+++ lib/book.rb
@@ -0,0 +1,16 @@
+class Book
+attr_reader :title,
+            :author,
+            :publication_date
+
+  def initialize(book)
+    @title = book[:title]
+    @author = book[:author_first_name] + ' ' + book[:author_last_name]
+    @publication_date = book[:publication_date]
+  end
+
+  def publication_year
+    array_year = publication_date.split(' ')
+    array_year[2]
+  end
+end
