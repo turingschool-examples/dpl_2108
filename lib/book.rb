2aa33dcb2b6b9cd48d56c4c48aac63f6aaf71d5b
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-16 14:48:35 -0600

    Feat - Add helper method for checking out books

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,22 @@
 class Book
 attr_reader :title,
             :author,
-            :publication_date
+            :publication_date,
+            :checked_out
 
   def initialize(book)
     @title = book[:title]
     @author = book[:author_first_name] + ' ' + book[:author_last_name]
     @publication_date = book[:publication_date]
+    @checked_out = 0
   end
 
   def publication_year
     array_year = publication_date.split(' ')
     array_year.last
+  end
+
+  def checked_out
+    @checked_out += 1
   end
 end
