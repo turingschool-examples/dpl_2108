90810f9664b4edb2a5016e7e7d487d4ed6e71769
Dione <dionew1@gmail.com>
2021-09-16 20:50:28 -0600

    Update 3iteration_spec.rb

--- harness/3iteration_spec.rb
+++ harness/3iteration_spec.rb
@@ -33,8 +33,14 @@ RSpec.describe 'Iteration 3' do
     jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
     professor = charlotte_bronte.write("The Professor", "1857")
     villette = charlotte_bronte.write("Villette", "1853")
+    charlotte_bronte.publish(jane_eyre)
+    charlotte_bronte.publish(professor)
+    charlotte_bronte.publish(villette)
+    
     harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
     mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    harper_lee.publish(mockingbird)
+    
     @dpl.add_author(charlotte_bronte)
     @dpl.add_author(harper_lee)
 
@@ -48,8 +54,13 @@ RSpec.describe 'Iteration 3' do
     jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
     professor = charlotte_bronte.write("The Professor", "1857")
     villette = charlotte_bronte.write("Villette", "1853")
+    charlotte_bronte.publish(jane_eyre)
+    charlotte_bronte.publish(professor)
+    charlotte_bronte.publish(villette)
+    
     harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
     mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    harper_lee.publish(mockingbird)
 
     expect(@dpl.publication_time_frame_for(charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
     expect(@dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
